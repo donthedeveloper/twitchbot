00f98f03171549cb70044882ac20824a32dacc12
Don <donhansen347@gmail.com>
2018-01-28 00:14:21 -0600

    set up basic express routing

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 require('dotenv').config();
-const irc = require('./irc');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+const chalk = require('chalk');
 
+const router = require('./server/routes');
+
+// middleware
+app.use(morgan('dev'));
+
+const nunjucksConfigure = nunjucks.configure('server/templates', {
+    noCache: true,
+    express: app
+});
+
+app.use(express.static('browser/public'));
+
+app.use('/', router);
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log(chalk.blue(`App is listening on port ${this.address().port}`));
+});
+
+const irc = require('./server/irc');
 irc.start();
